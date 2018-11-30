771807f49ddec4611ea17106c982c78a8fbcf3ed
pinkqy <694325192@qq.com>
2018-11-30 21:36:35 +0800

    reg

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,9 @@
 |
 */
 
-Route::get('/','StaticPagesController@home')->name('name');
+Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
-route::get('signup','UserController@create')->name('signup');
+Route::get('signup','UserController@create')->name('signup');
+Route::resource('users','UsersController');
