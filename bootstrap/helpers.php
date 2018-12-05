52959a644f61483c1e31d640b389a431b3b3e36e
pinkq <pinkqy520@gmail.com>
2018-12-05 10:45:12 +0000

    database fix

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,20 +3,21 @@ function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
         $url = parse_url(getenv("DATABASE_URL"));
+
         return $db_config = [
             'connection' => 'pgsql',
             'host' => $url["host"],
-            'databse' => substr($url["path"],1),
-            'username' => $url["user"],
-            'passwowd' => $url["pass"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
         ];
-    }else{
+    } else {
         return $db_config = [
-            'connection' =>env('DB_CONNECTION','mysql'),
-            'host' =>env('DB_HOST','localhost'),
-            'database' => env('DB_DATABASE','forge'),
-            'username' => env('DB_USERNAME','forge'),
-            'password' => env('DB_PASSWORD',''),
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
         ];
     }
 }
