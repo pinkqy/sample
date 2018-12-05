ddf6af0bfe839915db0e8ed5c9660140d850a8a1
pinkqy <694325192@qq.com>
2018-12-05 18:17:51 +0800

    Updated datebase configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,25 +8,39 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('users.create');
     }
-    public function show(User $user){
-        return view('users.show',compact('user'));
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
-    public function store(Request $request){
-        $this->validate($request, [
-            'name' => 'required|max:50',
-            'email' => 'required|email|unique:users|max:255',
-            'password'=>'required|confirmed|min:6'
-        ]);
-        $user = User::create([
-           'name' => $request->name,
-           'email' => $request->email,
-           'password'=>bcrypt($request->password),
-        ]);
+
+    public function store(Request $request)
+    {
+        $this->validate($request, ['name' => 'required|max:50', 'email' => 'required|email|unique:users|max:255', 'password' => 'required|confirmed|min:6']);
+        $user = User::create(['name' => $request->name, 'email' => $request->email, 'password' => bcrypt($request->password),]);
         Auth::login($user);
-        session()->flash('success','欢迎');
-        return redirect()->route('users.show',[$user]);
+        session()->flash('success', '欢迎');
+        return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, ['name' => 'required|max:50', 'password' => 'nullable|confirmed|min:6']);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        return redirect()->route('users.show', $user->id);
     }
 }
